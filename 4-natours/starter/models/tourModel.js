fc790af3e5d4eb924a40a5294734e12b4a9be17b
harshpatel1692 <16963646+harshpatel1692@users.noreply.github.com>
2020-08-15 15:05:06 -0500

    Mongoose Middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
         trim: true,
         unique: true
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration']
@@ -51,12 +53,54 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
-}, {
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
+},
+    //Schema definition and Schema options
+{
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
 
-}); //Schema definition and Schema options
+});
+
+// DOCUMENT MIDDLEWARE - called before .save() and .create()
+tourSchema.pre('save', function(next) {
+    // console.log(this)
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+/*
+//Middleware after save
+
+tourSchema.post('save', function (doc, next) {
+   console.log(doc);
+   next();
+});
+*/
+
+// QUERY MIDDLEWARE
+//tourSchema.pre('find', function (next) - can apply regex instead in the pre & post
+tourSchema.pre(/^find/, function (next) {
+    //Will ignore the secret tour
+    this.find( { secretTour: { $ne: true } } )
+    this.start = Date.now();
+    next();
+});
+tourSchema.post(/^find/, function (docs, next) {
+    console.log('Query took: ', Date.now() - this.start, 'ms');
+    next();
+})
+
+// AGGREGATION MIDDLEWARE - remove secret tour from aggregations
+tourSchema.pre('aggregate', function (next) {
+    //Add in the beginning of pipeline array
+    this.pipeline().unshift( { $match: { secretTour: { $ne: true } } } );
+    next();
+})
+
 
 // Used function() instead of => because using arrow doesn't get 'this' keyword
 // Cannot use virtual properties in query
