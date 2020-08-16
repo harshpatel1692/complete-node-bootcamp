eae8cae610eb068a59548ae3d6fc5702727927b4
harshpatel1692 <16963646+harshpatel1692@users.noreply.github.com>
2020-08-16 01:20:56 -0500

    Async - Unhandled Rejections Sync - Uncaught Exceptions

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+//Sync - Uncaught Exceptions - THIS HAS TO BE ON VERY TOP - START LISTENING TO EVENT AT THE VERY BEGINNING
+process.on('uncaughtException', err => {
+    console.log('Uncaught Exception');
+    console.log(err.name, err.message);
+    // process.exit(1); //code-1 is unhandled rejection; Abort all the pending request, etc
+    process.exit(1); //Need to crash the app as there is error in the code - example undefined vars
+});
+
 //Need to run this before executing app.js
 dotenv.config({path: './config.env'}); //read of env file has to happen once and the process has access to the variables. i.e all the files in the project
 
@@ -17,7 +26,7 @@ mongoose.connect(DB, {
 }).then(() => {
     //con.connections;
     console.log('Connection Successful');
-})
+});
 
 /*const testTour = new Tour({
     name: 'The Park Camper',
@@ -35,6 +44,16 @@ testTour
 // console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port: ${port}`);
+});
+
+//Async - Unhandled Rejections; Sync error exception at top.
+process.on('unhandledRejection', err => {
+    console.log('Unhandled Rejection');
+    console.log(err.name, err.message);
+    // process.exit(1); //code-1 is unhandled rejection; Abort all the pending request, etc
+    server.close(() => {
+        process.exit(1);
+    });
 });
