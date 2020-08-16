b4e46536e8cce3bb2806710c939dd4495e469794
harshpatel1692 <16963646+harshpatel1692@users.noreply.github.com>
2020-08-16 01:55:55 -0500

    User Schema

---
+++ 4-natours/starter/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A user name is required'],
+
+    },
+    email: {
+        type: String,
+        required: [true, 'please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minLength: 8
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password']
+    }
+});
+
+//Models starts with capital name
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
